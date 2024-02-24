29d8d27cb2adc8217df3186bfa589d13d4505e88
younes101020 <senyoupl@icloud.com>
2024-02-24 22:52:20 +0100

    feat: add front hardcoded dependencies installation

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
@@ -17,16 +17,20 @@ const figlet_1 = __importDefault(require("figlet"));
 const commander_1 = require("commander");
 const fs_1 = __importDefault(require("fs"));
 const child_process_1 = require("child_process");
+const util_1 = __importDefault(require("util"));
+const execProm = util_1.default.promisify(child_process_1.exec);
 const program = new commander_1.Command();
 console.log(figlet_1.default.textSync("Youp CLI"));
 program
     .version("1.0.0")
     .description("CLI for bootstraping fullstack modern web project")
-    .option("-s, --start <name>", "Create project")
+    .option("-s, --start <project_name>", "create new fullstack project")
     .parse(process.argv);
 const options = program.opts();
 if (options.start) {
-    const name = typeof options.start === "string" ? options.start : "fullstack_app";
+    const name = typeof options.start === "string"
+        ? `../${options.start}`
+        : "../fullstack_app";
     bootstrapNext(name);
 }
 function bootstrapNext(name) {
@@ -35,11 +39,11 @@ function bootstrapNext(name) {
             if (!fs_1.default.existsSync(name)) {
                 fs_1.default.mkdirSync(name);
             }
-            (0, child_process_1.exec)("npx create-next-app@latest", { cwd: name }, (error, _, stderr) => {
-                if (error || stderr) {
-                    throw new Error("Can't create root folder");
-                }
+            const { stdout, stderr } = yield execProm("yarn add next@latest react@latest react-dom@latest", {
+                cwd: name,
             });
+            console.log("stdout:", stdout);
+            console.log("stderr:", stderr);
         }
         catch (error) {
             console.error("Error occurred while bootstrap NextJS!", error);
